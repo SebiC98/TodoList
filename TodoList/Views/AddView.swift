76433e4ad1b3a0822c31b8ba999298ae067acd72
SebiC98 <sebastiancioata98@gmail.com>
2023-03-15 21:40:02 +0200

    Create a List of Todo items and Add an Item

---
+++ TodoList/Views/AddView.swift
@@ -0,0 +1,46 @@
+//
+//  AddView.swift
+//  TodoList
+//
+//  Created by Sebastian Cioată on 15.03.2023.
+//
+
+import SwiftUI
+
+struct AddView: View {
+    
+    @State var textFieldText: String = ""
+    
+    var body: some View {
+        ScrollView{
+            VStack {
+                TextField("Type something here...", text: $textFieldText)
+                    .padding(.horizontal)
+                    .frame(height: 55)
+                    .background(Color.gray)
+                    .cornerRadius(10)
+                Button(action: {
+                    
+                }) {
+                    Text("Save".uppercased())
+                        .foregroundColor(.white)
+                        .font(.headline)
+                        .frame(height: 55)
+                        .frame(maxWidth: .infinity)
+                        .background(Color.accentColor)
+                        .cornerRadius(10)
+                }
+            }
+            .padding(14)
+        }
+        .navigationTitle("Add an Item 🖊️")
+    }
+}
+
+struct AddView_Previews: PreviewProvider {
+    static var previews: some View {
+        NavigationView{
+            AddView()
+        }
+    }
+}
